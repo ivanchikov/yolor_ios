1089df22ae295c8531459e365e394afc4c43acac
ivanfromthailand <ivanfromthailand@gmail.com>
2026-03-24 17:47:28 +0800

    Initial Commit

---
+++ yolor/yolorApp.swift
@@ -0,0 +1,17 @@
+//
+//  yolorApp.swift
+//  yolor
+//
+//  Created by Han Xu on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct yolorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
